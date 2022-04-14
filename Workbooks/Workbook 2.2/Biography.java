da0db2bd8f62f3be2eab3bd423ece620e337010c
Farhad Siraj <farhad.siraj@ibm.com>
2022-04-14 13:06:13 -0400

    Added Workbooks and Challenges

---
+++ Workbooks/Workbook 2.2/Biography.java
@@ -0,0 +1,19 @@
+public class Biography {
+    public static void main(String[] args) {
+
+        String name = "Farhad";
+        int age = 31;
+        String country = "USA";
+        String eSport = "League of Legends";
+        int hour = 3;
+        String game = "Chrono Cross";
+        String subject = "History";
+        char grade = 'A';
+
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from " + country + ".");
+        System.out.println("My favourite e-sport is " + eSport + ". I play for " + hour + " hours a day.");
+        System.out.println("When I'm tired, I like to play " + game + ".");
+        System.out.println("In school, my favourite subject was " + subject + ", I scored an " + grade + ".");
+
+    }
+}
