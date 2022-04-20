894a720642c06f87f065c340b0f3b8e518c741f8
Farhad Siraj <farhadsiraj@gmail.com>
2022-04-20 16:26:05 -0400

    Completed Workbook 5.8

--- Workbooks/Workbook 5.8/Guess.java
+++ Workbooks/Workbook 5.8/Guess.java
@@ -1,23 +1,32 @@
 import java.util.Scanner;
 
-public class Guess  {
+public class Guess {
     public static void main(String[] args) {
 
-       /* Task 1 
-            1. Store a "secret" number between 1 and 5. 
-            2. Then, prompt the user to enter a guess.
-       */
-       System.out.print("I chose a number between 1 and 5. Try to guess it: ");
-        
-       Scanner scan = new Scanner(System.in);
+        /*
+         * Task 1
+         * 1. Store a "secret" number between 1 and 5.
+         * 2. Then, prompt the user to enter a guess.
+         */
+        int secret = 5;
+        System.out.print("I chose a number between 1 and 5. Try to guess it: ");
 
+        Scanner scan = new Scanner(System.in);
+        int guess = scan.nextInt();
+        /*
+         * Task 2
+         * 1. Set up a loop that keeps running while the user is incorrect
+         * Every time they get it wrong, write 'Guess again: '
+         * 
+         * 2. Once they guess it, print: You got it!
+         */
 
-        /* Task 2 
-            1. Set up a loop that keeps running while the user is incorrect
-               Every time they get it wrong, write 'Guess again: '
+        while (guess != secret) {
+            System.out.println("Guess again");
+            guess = scan.nextInt();
+        }
 
-            2. Once they guess it, print: You got it!
-        */        
+        System.out.println("You got it!");
         scan.close();
     }
 
