28b147e86053adb4479098ed7915d669990f7299
Farhad Siraj <farhadsiraj@gmail.com>
2022-04-20 16:39:11 -0400

    Completed Workbook 5.9

--- Workbooks/Workbook 5.9/SignIn.java
+++ Workbooks/Workbook 5.9/SignIn.java
@@ -4,28 +4,43 @@ public class SignIn {
     public static void main(String[] args) {
         String username = "Samantha";
         String password = "Java <3";
-        
-        /* Task 1 
-            1. Pick up a username and password from the user.
-       */
+
+        /*
+         * Task 1
+         * 1. Pick up a username and password from the user.
+         */
         Scanner scan = new Scanner(System.in);
         System.out.println("\nWelcome to Javagram! Sign in below\n");
         System.out.print("• Username: ");
-        //pick up username
+        // pick up username
+        String inputUsername = scan.nextLine();
         System.out.print("• Password: ");
-        //pick up password
+        // pick up password
+        String inputPassword = scan.nextLine();
 
-        /* Task 2 
-            1. Set up a loop that keeps running while the username OR password is incorrect. 
-               When they get it wrong:
-                •  println: \nIncorrect, please try again!\n
-                •  get them to enter their username and password again
-            2. After they enter the correct information, print: 
-                   \nSign in successful. Welcome!
-       */
-
+        System.out.println(inputUsername);
+        System.out.println(inputPassword);
+        /*
+         * Task 2
+         * 1. Set up a loop that keeps running while the username OR password is
+         * incorrect.
+         * When they get it wrong:
+         * • println: \nIncorrect, please try again!\n
+         * • get them to enter their username and password again
+         * 2. After they enter the correct information, print:
+         * \nSign in successful. Welcome!
+         */
+        while (!password.equals(inputPassword) || !username.equals(inputUsername)) {
+            System.out.println("\nIncorrect, please try again!\n");
+            System.out.print("• Username: ");
+            // pick up username
+            inputUsername = scan.nextLine();
+            System.out.print("• Password: ");
+            // pick up password
+            inputPassword = scan.nextLine();
+        }
+        System.out.println("\nSign in successful. Welcome!");
         scan.close();
 
-        
     }
 }
