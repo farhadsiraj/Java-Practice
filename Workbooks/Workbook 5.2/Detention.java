894d439d30642378a5d0bf2cc92359260850850c
Farhad Siraj <farhadsiraj@gmail.com>
2022-04-20 15:12:00 -0400

    Completed Workbook 5.2

--- Workbooks/Workbook 5.2/Detention.java
+++ Workbooks/Workbook 5.2/Detention.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class Detention {
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
         System.out.println("Hi Bart. I can write lines for you.\nWhat do you want me to write?");
-        //Task 1 – Scan for the nextLine(), and print it 99 times. 
-        
+        // Task 1 – Scan for the nextLine(), and print it 99 times.
+        String response = scan.nextLine();
+
+        for (int i = 1; i <= 99; i++) {
+            System.out.println(i + ". " + response);
+        }
     }
 }
