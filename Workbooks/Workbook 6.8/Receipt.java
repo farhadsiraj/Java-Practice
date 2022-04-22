ddffe333221de8631f375dd261d44f9abc022b99
Farhad Siraj <farhadsiraj@gmail.com>
2022-04-22 15:32:27 -0400

    Completed Workbook 6.8

--- Workbooks/Workbook 6.8/Receipt.java
+++ Workbooks/Workbook 6.8/Receipt.java
@@ -3,15 +3,23 @@ import java.util.Arrays;
 public class Receipt {
     public static void main(String[] args) {
 
-        //Task 1: Create two arrays (see Workbook article)
+        // Task 1: Create two arrays (see Workbook article)
+
+        String[] appleNames = { "Gala", "Granny Smith", "Macintosh" };
+        Double[] applePrices = { 1.99, 1.49, 1.29 };
 
         System.out.println("Here's your receipt:\n");
-        /* Task 2
-            Use a for loop to print each apple and the corresponding price.
-            
-            for {
-                println(\t<apple i >: $<price i >)
-            }
-        */
+        /*
+         * Task 2
+         * Use a for loop to print each apple and the corresponding price.
+         * 
+         * for {
+         * println(\t<apple i >: $<price i >)
+         * }
+         */
+
+        for (int i = 0; i < appleNames.length; i++) {
+            System.out.println("\t" + appleNames[i] + ": $" + applePrices[i]);
+        }
     }
 }
