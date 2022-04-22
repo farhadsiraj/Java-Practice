cce15e5411081c4a93d96c13c7ae72806bb65dfa
Farhad Siraj <farhadsiraj@gmail.com>
2022-04-22 13:07:46 -0400

    Completed Workbook 6.1

---
+++ Arrays/IntroToArrays.java
@@ -0,0 +1,16 @@
+public class IntroToArrays {
+    public static void main(String[] args) {
+        String[] kingdoms = { "Mercia", "Wessex", "Northumbria", "East Anglia" };
+
+        // printing will not display the array,
+        // instead it will only print the reference to the array
+        System.out.println(kingdoms);
+
+        // You can print out each element using the index
+        System.out.println(kingdoms[0]);
+        System.out.println(kingdoms[1]);
+        System.out.println(kingdoms[2]);
+        System.out.println(kingdoms[3]);
+
+    }
+}
