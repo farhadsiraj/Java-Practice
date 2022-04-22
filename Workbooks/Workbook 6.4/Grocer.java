45b264ed87e78d1b20d099f5fa3da509fa01e254
Farhad Siraj <farhadsiraj@gmail.com>
2022-04-22 14:19:50 -0400

    Completed Workbook 6.4

--- Workbooks/Workbook 6.4/Grocer.java
+++ Workbooks/Workbook 6.4/Grocer.java
@@ -1,20 +1,29 @@
+import java.util.Scanner;
+
 public class Grocer {
     public static void main(String[] args) {
 
-        String[] store = {"apples", "bananas", "candy", "chocolate", "coffee", "tea"};
+        String[] store = { "apples", "bananas", "candy", "chocolate", "coffee", "tea" };
         System.out.println("\nWelcome to Java Grocers. ");
         System.out.println("What can I help you find?\n");
-        //  Task 1: Set up Scanner, and pick up the user's response.  
+        // Task 1: Set up Scanner, and pick up the user's response.
 
-        
+        Scanner scan = new Scanner(System.in);
+        String request = scan.next();
+
         for (int i = 0; i < store.length; i++) {
-          /* Task 2  
-                if element equals user's response  {
-                    println: \nWe have that in aisle:  <index>
-                    break;
-                }
-          */
-        }
+            /*
+             * Task 2
+             * if element equals user's response {
+             * println: \nWe have that in aisle: <index>
+             * break;
+             * }
+             */
 
+            if (store[i].equals(request)) {
+                System.out.println("\nWe have that in aisle: " + i);
+            }
+        }
+        scan.close();
     }
 }
