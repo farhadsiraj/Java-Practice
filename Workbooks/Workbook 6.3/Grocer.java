700f38988405dbffd4ee48bd0f691aab89b6aaa3
Farhad Siraj <farhadsiraj@gmail.com>
2022-04-22 14:12:20 -0400

    Completed Workbook 6.3

--- Workbooks/Workbook 6.3/Grocer.java
+++ Workbooks/Workbook 6.3/Grocer.java
@@ -2,21 +2,36 @@ import java.util.Scanner;
 
 public class Grocer {
     public static void main(String[] args) {
-        
-        /* Task 1: Create an array that stores each aisle:
-             apples – bananas – candy – chocolate – coffee – tea
 
-             Then, use a for loop to print each element in the array. 
-        */
- 
-        /* Task 2:
-           1. System.out.println("\nDo you sell coffee?");
+        /*
+         * Task 1: Create an array that stores each aisle:
+         * apples – bananas – candy – chocolate – coffee – tea
+         * 
+         * Then, use a for loop to print each element in the array.
+         */
 
-           2. Using the for loop from task 1:
-            • check if Java Grocer sells coffee.
-            • if so, break the loop and print: \nWe have that in aisle: <index>
+        String[] grocery = { "apples", "bananas", "candy", "chocolate", "coffee", "tea" };
 
-        */
-    
+        for (int i = 0; i < grocery.length; i++) {
+            System.out.println(grocery[i]);
+        }
+
+        /*
+         * Task 2:
+         * 1. System.out.println("\nDo you sell coffee?");
+         * 
+         * 2. Using the for loop from task 1:
+         * • check if Java Grocer sells coffee.
+         * • if so, break the loop and print: \nWe have that in aisle: <index>
+         * 
+         */
+
+        System.out.println("\nDo you sell coffee?");
+        for (int i = 0; i < grocery.length; i++) {
+            if (grocery[i] == "coffee") {
+                System.out.println("\nWe have that in aisle: " + i);
+            }
+        }
+
     }
 }
