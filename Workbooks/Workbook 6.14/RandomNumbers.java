8ed27d353542e45ba11c1e4e08b446392f64840d
Farhad Siraj <farhadsiraj@gmail.com>
2022-04-25 13:04:46 -0400

    Completed Workbook 6.14

--- Workbooks/Workbook 6.14/RandomNumbers.java
+++ Workbooks/Workbook 6.14/RandomNumbers.java
@@ -1,37 +1,57 @@
 public class RandomNumbers {
     public static void main(String[] args) {
         // Task 3 – Create a 2D array with 100 rows and 10 columns.
-
-
-        /** Task 4
+        int[][] table = new int[100][10];
+        /**
+         * Task 4
          * 
-         * 1. Using a nested loop, populate the array with random numbers. 
-         * 2. Then, pass the updated array into print2DArray.  
+         * 1. Using a nested loop, populate the array with random numbers.
+         * 2. Then, pass the updated array into print2DArray.
          * 
          */
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+                table[i][j] = randomNumber();
+            }
+        }
+        print2DArray(table);
 
-
-        
     }
 
-    /** Task 1
+    /**
+     * Task 1
      * Function name: randomNumber.
      * 
      * @return randomNumber (int)
      * 
-     * Inside the function:
-     *    1. return a random number between 1 and 100. 
+     *         Inside the function:
+     *         1. return a random number between 1 and 100.
      */
 
+    public static int randomNumber() {
+        double num = Math.random() * 100;
+        return (int) num;
+    }
 
-    /** Task 2
+    /**
+     * Task 2
      * Function name: print2DArray
+     * 
      * @param array ( int[][] )
      *
-     * Inside the function:
+     *              Inside the function:
      *
-     *   1. prints the array such that:
-     *        • Each row takes up one line.
-     *        • There's a space between each value.
+     *              1. prints the array such that:
+     *              • Each row takes up one line.
+     *              • There's a space between each value.
      */
+
+    public static void print2DArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
 }
