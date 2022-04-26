ae06e18f617d02c735958c7adce6966021d9350b
Farhad Siraj <farhadsiraj@gmail.com>
2022-04-26 13:17:22 -0400

    Completed Workbook 7.2

--- Workbooks/Workbook 7.1/airline-reservation-system/Main.java
+++ Workbooks/Workbook 7.1/airline-reservation-system/Main.java
@@ -1,12 +1,15 @@
+import java.security.Permissions;
+
 public class Main {
-  
+
     public static void main(String[] args) {
 
+        Person person = new Person("Bruce Wayne", "American", "02/19/1972", 1);
 
-      
-      
-      
+        System.out.println("Name: " + person.name + "\n"
+                + "Nationality: " + person.nationality + "\n"
+                + "Date of Birth: " + person.dateOfBirth + "\n"
+                + "Seat Number: " + person.seatNumber + "\n");
     }
-  
-  
+
 }
--- Workbooks/Workbook 7.1/airline-reservation-system/Person.java
+++ Workbooks/Workbook 7.1/airline-reservation-system/Person.java
@@ -2,6 +2,13 @@ public class Person {
     String name;
     String nationality;
     String dateOfBirth;
-    Stringp[] passport;
+    String[] passport;
     int seatNumber;
+
+    public Person(String name, String nationality, String dateOfBirth, int seatNumber) {
+        this.name = name;
+        this.nationality = nationality;
+        this.dateOfBirth = dateOfBirth;
+        this.seatNumber = seatNumber;
+    }
 }
