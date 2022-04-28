106a018b694f1487252ebca2a4ba7760a149cca0
Farhad Siraj <farhadsiraj@gmail.com>
2022-04-28 14:58:15 -0400

    Completed Workbook 7.9

--- Workbooks/Workbook 7.xx/airline-reservation-system/Airline.java
+++ Workbooks/Workbook 7.xx/airline-reservation-system/Airline.java
@@ -15,4 +15,31 @@ public class Airline {
         this.seats[person.getSeatNumber() - 1] = new Person(person);
 
     }
+
+    public void createReservation(Person person) {
+        while (this.seats[person.getSeatNumber() - 1] != null) {
+            System.out.println("\n" + person.getName() + ", seat: " + person.getSeatNumber()
+                    + " is already taken. Please choose another seat.\n");
+            person.chooseSeat();
+        }
+
+        int index = person.getSeatNumber() - 1;
+        this.seats[index] = new Person(person);
+        System.out.println("Thank you " + person.getName() + " for flying with Java airlines. Your seat number is "
+                + person.getSeatNumber() + ".\n");
+    }
+
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < this.seats.length; i++) {
+            if (seats[i] != null) {
+                temp += seats[i].toString();
+                temp += "\n\n";
+            } else {
+                temp += "Seat " + (i + 1) + " is empty.";
+                temp += "\n\n";
+            }
+        }
+        return temp;
+    }
 }
--- Workbooks/Workbook 7.xx/airline-reservation-system/Main.java
+++ Workbooks/Workbook 7.xx/airline-reservation-system/Main.java
@@ -17,32 +17,20 @@ public class Main {
         Airline airline = new Airline();
 
         for (int i = 0; i < people.length; i++) {
-            airline.setPerson(people[i]);
+
+            Person person = people[i];
+
+            if (person.applyPassport() == true) {
+
+                person.setPassport();
+                airline.createReservation(person);
+
+            } else {
+                System.out.println("We're sorry " + person.getName() + ". We cannot process your application.");
+            }
+
         }
+        System.out.println("****************** RESERVATIONS COMPLETE! ******************\n");
+        System.out.println(airline);
     }
-
-    // public static void main(String[] args) {
-
-    // Person person = new Person("Bruce Wayne", "American", "02/19/1972", 1);
-
-    // if (person.applyPassport() == true) {
-    // person.setPassport();
-    // } else {
-    // System.out.println("We're sorry " + person.getName() + ". We cannot process
-    // your application.");
-    // }
-
-    // // person.chooseSeat();
-
-    // // // Person persona = new Person(person);
-
-    // // // persona.setName("Batman");
-    // // // persona.setNationality("Unknown");
-    // // // persona.setDateOfBirth("Unknown");
-    // // // persona.setPassport(new String[] { "Unknown", "Unknown", "Unknown" });
-    // // // persona.setSeatNumber(2);
-
-    // System.out.println(person);
-    // }
-
 }
