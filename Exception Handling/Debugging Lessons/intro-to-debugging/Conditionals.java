8786d51d27fde4e6ac069e7aeaac98cd4381b30e
Farhad Siraj <farhadsiraj@gmail.com>
2022-05-05 15:30:54 -0400

    Completed Workbook 8.2

--- Exception Handling/Debugging Lessons/intro-to-debugging/Conditionals.java
+++ Exception Handling/Debugging Lessons/intro-to-debugging/Conditionals.java
@@ -2,20 +2,26 @@ public class Conditionals {
     public static void main(String[] args) {
         String option = "";
 
-        int random = (int) Math.random() * 1 + 3;
-        
-        switch (random) {
-            case 1: option = "hit"; 
-            case 2: option = "stay";
-            case 3: option = "fold";
-            default: option = "This should never get called";
-        }
-     
+        int random = (int) (Math.random() * 3 + 1);
 
-        if (!option.equals("hit") || !option.equals("stay")) {
+        switch (random) {
+            case 1:
+                option = "hit";
+                break;
+            case 2:
+                option = "stay";
+                break;
+            case 3:
+                option = "fold";
+                break;
+            default:
+                option = "This should never get called";
+        }
+
+        if (!option.equals("hit") && !option.equals("stay")) {
             System.out.println("fold");
         } else {
-            System.out.println(option); 
-       } 
+            System.out.println(option);
+        }
     }
 }
