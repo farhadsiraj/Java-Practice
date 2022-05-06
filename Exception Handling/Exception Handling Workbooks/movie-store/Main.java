6c4d4d21d39d7b0aeb63fb136c570c5d24b5d84b
Farhad Siraj <farhadsiraj@gmail.com>
2022-05-06 16:30:18 -0400

    Re-organized folder structure

--- Exception Handling/Exception Handling Workbooks/movie-store/Main.java
+++ Exception Handling/Exception Handling Workbooks/movie-store/Main.java
@@ -1,6 +1,26 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import models.Movie;
+import models.Store;
+
 public class Main {
+    
+
+    static Store store = new Store();
+
     public static void main(String[] args) {
         System.out.println("\n********************JAVA VIDEO STORE********************\n");
+        try {
+            loadMovies("movies.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("MOVIES LOADED\n\n");
+            System.out.println(store);
+            manageMovies();
+        }
 
     }
 
@@ -15,6 +35,41 @@ public class Main {
      *   • 3. call close() from the Scanner object.
      */
 
+     public static void manageMovies() {
+         Scanner scan = new Scanner(System.in);
+         while (true) {
+            System.out.println("\nWould you like to \n\ta) purchase\n\tb) rent \n\tc) return.");
+            String response = scan.nextLine();
+
+            if (!(response.equals("a") || response.equals("b") || response.equals("c"))) {
+                scan.close();
+                break;
+            }
+
+            System.out.print("Enter the name of the movie: ");
+            String name = scan.nextLine();
+            if (store.getMovie(name) == null) {
+                System.out.println("\n\nThe input you provided is not valid. Please try again\n");
+                continue;
+            }
+
+            switch (response) {
+                case "a":
+                 if (!(store.getMovie(name).isAvailable())) {
+                    System.out.println("\n\n\n\nThe movie is not available for purchase. Please try again\n");
+                    continue;
+                 }
+                 store.action(name, "sell"); break;
+                case "b": store.action(name, "rent"); break;
+                case "c": store.action(name, "return"); break;
+            }
+            System.out.println("\n\nUPDATED MOVIES\n\n" + store);
+         }
+     }
+
+
+
+
 
     /**
      * Name: loadMovies
@@ -24,7 +79,19 @@ public class Main {
      * Inside the function:
      *   • 1. loads movies from <fileName>.txt.
      *   • 2. adds all movies to the store object's movie field.
-     *        Hint: You will need to 'split' a String into three Strings.
      */
+
+     public static void loadMovies(String fileName) throws FileNotFoundException {
+         FileInputStream fis = new FileInputStream(fileName);
+         Scanner scanFile = new Scanner(fis);
+
+         while (scanFile.hasNextLine()) {
+             String line = scanFile.nextLine();
+             String[] words = line.split("--");
+             store.addMovie(new Movie(words[0], words[1], Double.parseDouble(words[2])));
+         }
+         scanFile.close();
+     }
+
 
 }
